6530ac9d56f75a681529769cb492ceb8a127e507
juniordbz <franciscojunior02@hotmail.com.br>
2026-01-18 14:49:12 -0300

    config inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,7 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
-    >
-      <span>Hello world!</span>
+    <div className="min-h-screen flex items-center justify-center">
+      <span className="text-4xl">Hello world!</span>
     </div>
   );
 }
